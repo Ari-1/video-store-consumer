b2c254bb5e4bff58f27a70d2be86ea4cd8829c8c
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-06-18 16:55:05 -0700

    Updated Library.js component to render the URL with axios. Created Movie.js component to render the movie title and poster image.

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Movie from './Movie';
+
+const BASE_URL = "http://localhost:3000/movies"
+
+class Library extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('In componentsDidMount');
+
+    // this.props.updateStatusCallback('Loading...', 'success');
+
+    axios.get(BASE_URL)
+    .then((response) => {
+      console.log('Success!');
+      console.log(response.data)
+
+      // this.props.updateStatusCallback('Successfully loaded all movies!', 'success');
+
+      const movies = response.data;
+      this.setState({ movies: movies });
+    })
+    .catch((error) => {
+      console.log('Error :(');
+      console.log(error);
+
+      // this.props.updateStatusCallback(error.message, 'error');
+    });
+  }
+
+  render() {
+
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie key={index}
+      index={index}
+      title={movie.title}
+      image={movie.image_url}
+      />
+    });
 
 
-const Library = () => (
-  <div>
-    <h2>Library</h2>
-  </div>
-)
+    return (
+      <section>
+
+      <div>
+        <h2>{movies}</h2>
+      </div>
+
+      </section>
+    );
+  }
+
+}
 
 export default Library;
---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Movie extends Component {
+
+  render() {
+
+    return (
+      <section>
+
+      <div>
+        <h1>
+          { this.props.title }
+        </h1>
+
+        <img
+          src={this.props.image} alt={this.props.title}
+        />
+      </div>
+      </section>
+    )
+  }
+}
+
+Movie.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+};
+
+export default Movie;
